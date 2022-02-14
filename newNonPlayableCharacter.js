4b391b742a67c7fd7a2a43b4d7ffb516686911f5
Nghia Nguyen <aihgn224@gmail.com>
2022-02-13 20:43:10 -0800

    Completed Assignment

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -4,6 +4,12 @@ function newNonPlayableCharacter(x, y) {
     
     let direction = null;
 
+    function sleep(time){
+        return new Promise(resolve => {
+            setTimeout(resolve, time)
+        })  
+    }
+
     function moveCharacter() {
         if (direction === 'west') {
             x -= 1
@@ -23,25 +29,35 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time);
+        stop()
     }
-
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        await sleep(time);
+        stop()
     }
 
-    function walkWest() {
+
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time);
+        stop()
     }
 
-    function walkSouth() {
+
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time);
+        stop()
     }
+
 
     function stop() {
         direction = null
